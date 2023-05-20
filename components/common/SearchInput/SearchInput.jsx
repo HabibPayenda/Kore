511d7fcc2cf32b66b598fe8a5edccd046e33ed9d
Habib <habiburahmanpayenda@gmail.com>
2023-05-20 16:45:55 +0430

    Adds SearchInput component

---
+++ components/common/SearchInput/SearchInput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import styles from "./searchInput";
+
+const SearchInput = ({ value, placeholder, onChange }) => {
+  return (
+    <View style={styles.container}>
+      <Feather name="search" size={24} color="#ccc" style={styles.icon} />
+      <TextInput
+        style={styles.input}
+        value={value}
+        placeholder={placeholder}
+        onChangeText={onChange}
+      />
+    </View>
+  );
+};
+
+export default SearchInput;
---
+++ components/common/SearchInput/searchInput.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native-web";
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f7f7f7",
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+    color: "#333",
+  },
+});
+
+export default styles;
