963ecd8cb30166a7b37c4b8156c28427759fc9ed
Habib <habiburahmanpayenda@gmail.com>
2023-05-22 23:47:12 +0430

    Adds styles to PriceFilter

--- components/carFilter/PriceFilter/PriceFilter.jsx
+++ components/carFilter/PriceFilter/PriceFilter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Slider from "@react-native-community/slider";
+import { COLORS, FONT, SIZES } from "../../../constants";
 /*
  A component that allows users to choose a price range using a slider.
  */
@@ -17,7 +18,11 @@ const PriceFilter = ({ onPriceChange }) => {
   return (
     <View style={styles.container}>
       <View style={styles.sliderItem}>
-        <Text style={styles.label}>${lowestPrice}</Text>
+        <Text style={styles.label}>
+          {lowestPrice}
+          {"  "}
+          {lowestPrice > 1 ? "لکه" : "لک"}
+        </Text>
         <View style={styles.sliderContainer}>
           <Slider
             style={styles.slider}
@@ -31,6 +36,25 @@ const PriceFilter = ({ onPriceChange }) => {
           <Text style={styles.title}>ټیټه بیه:</Text>
         </View>
       </View>
+      <View style={styles.sliderItem}>
+        <Text style={styles.label}>
+          {highestPrice}
+          {"  "}
+          {highestPrice > 1 ? "لکه" : "لک"}
+        </Text>
+        <View style={styles.sliderContainer}>
+          <Slider
+            style={styles.slider}
+            minimumValue={lowestPrice}
+            maximumValue={20}
+            step={1}
+            value={highestPrice}
+            onValueChange={(value) => setHighestPrice(value)}
+          />
+
+          <Text style={styles.title}>لوړه بیه:</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -38,27 +62,37 @@ const PriceFilter = ({ onPriceChange }) => {
 const styles = StyleSheet.create({
   container: {
     marginBottom: 10,
-    width: 200,
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "center",
+    paddingHorizontal: SIZES.xLarge,
   },
   title: {
-    fontWeight: "bold",
     fontSize: 16,
     marginBottom: 5,
+    fontFamily: FONT.regular,
   },
   sliderContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    flex: 1,
   },
   label: {
     fontSize: 14,
+    fontFamily: FONT.regular,
   },
   slider: {
     flex: 1,
     marginHorizontal: 10,
+  },
+  sliderItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    height: 80,
+    width: 300,
   },
 });
 
