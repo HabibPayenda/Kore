42306bc1a928a52be3a1c9049fb3a3da82380b1e
Habib <habiburahmanpayenda@gmail.com>
2023-05-24 14:07:10 +0430

    Fixes shadow styles in NotificationCard component

--- components/notifications/NotificationCard/NotificationCard.jsx
+++ components/notifications/NotificationCard/NotificationCard.jsx
@@ -29,11 +29,11 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 1,
     marginVertical: 8,
     marginHorizontal: 16,
     paddingHorizontal: 16,
