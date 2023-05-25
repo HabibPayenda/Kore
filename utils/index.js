a425fdf4274b01aadfdb4a6e4d7b58763f49218d
Habib <habiburahmanpayenda@gmail.com>
2023-05-25 11:36:14 +0430

    Adds homesSlice PropertiesApi

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,39 @@
 export const checkImageURL = (url) => {
-    if (!url) return false
-    else {
-        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
-        return pattern.test(url);
-    }
+  if (!url) return false;
+  else {
+    const pattern = new RegExp(
+      "^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
 };
 
+import axios from "axios";
+import { api } from "../env";
+
+const PropertiesApi = axios.create({
+  baseURL: api,
+  headers: {
+    Accept: "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    "Content-Type": "application/json",
+  },
+});
+
+PropertiesApi.interceptors.request.use(
+  async (config) => {
+    let token = localStorage.getItem("token");
+    token = token.replace(/['"]+/g, "");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error.response.message);
+  }
+);
+
+export default PropertiesApi;
