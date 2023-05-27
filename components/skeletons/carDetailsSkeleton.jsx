6ceb853c393e360da5dcffd1bda86bfaabec96f6
Habib <habiburahmanpayenda@gmail.com>
2023-05-27 11:26:28 +0430

    Adds basic styles and layout to CarDetailsSkeleton component

--- components/skeletons/carDetailsSkeleton.jsx
+++ components/skeletons/carDetailsSkeleton.jsx
@@ -3,17 +3,24 @@ import { View, Text, Image, StyleSheet } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import SkeletonPlaceholder from "expo-react-native-skeleton-placeholder";
+import { SIZES } from "../../constants";
 const CarDetailsSkeleton = () => {
   return (
     <SafeAreaView>
       <SkeletonPlaceholder borderRadius={4}>
-        <View style={{ flexDirection: "column", alignItems: "center" }}>
+        <View style={styles.container}>
           <View style={styles.imageContainer} />
-          <View style={{ marginLeft: 20 }}>
-            <Image style={{ width: 120, height: 20 }} />
-            <Text style={{ marginTop: 6, fontSize: 14, lineHeight: 18 }}>
-              Hello world
-            </Text>
+          <View style={styles.detailsContainer}>
+            <View style={styles.detailsHeader}>
+              <View style={styles.item} />
+              <View style={styles.item} />
+              <View style={styles.item} />
+              <View style={styles.item} />
+              <View style={styles.item} />
+            </View>
+            <View style={styles.detailsContent}>
+              <Text style={styles.detailsText} />
+            </View>
           </View>
         </View>
       </SkeletonPlaceholder>
@@ -22,9 +29,41 @@ const CarDetailsSkeleton = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: SIZES.large,
+  },
   imageContainer: {
     width: "100%",
     height: 300,
+  },
+  detailsContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "50%",
+  },
+  detailsHeader: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    gap: SIZES.large,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  item: {
+    width: "30%",
+    height: 30,
+  },
+  detailsContent: {
+    flex: 4,
+    padding: SIZES.large,
+    height: "100%",
+    width: "100%",
+  },
+  detailsText: {
+    height: "100%",
+    width: "100%",
   },
 });
 
