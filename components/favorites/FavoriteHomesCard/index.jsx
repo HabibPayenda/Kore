0cb23673e30cceeec53886d8929f82e7a7bb9eb9
Habib <habiburahmanpayenda@gmail.com>
2023-06-07 18:51:01 +0430

    Adds styles to FavoriteHomesCard

--- components/favorites/FavoriteHomesCard/index.jsx
+++ components/favorites/FavoriteHomesCard/index.jsx
@@ -16,24 +16,29 @@ const FavoriteHomesCard = ({ home }) => {
         resizeMethod="resize"
       />
       <View style={styles.infoContainer}>
-        <Text>{home?.property?.name}</Text>
+        <View style={styles.infoHeaderContainer}>
+          <Text>{home?.property?.name}</Text>
+          <Text>{home?.property?.description}</Text>
+          <View
+            style={[
+              styles.availabilityCircle,
+              {
+                backgroundColor:
+                  home?.property?.availability_status === "available"
+                    ? "green"
+                    : red,
+              },
+            ]}
+          ></View>
+        </View>
         <View style={styles.detailsContainer}>
           <View style={styles.infoItem}>
             <Feather name="home" size={16} color="#333" style={styles.icon} />
-            <Text style={styles.infoText}>{rooms}</Text>
+            <Text style={styles.infoText}>{"4"}</Text>
           </View>
           <View style={styles.infoItem}>
             <Feather name="square" size={16} color="#333" style={styles.icon} />
-            <Text style={styles.infoText}>{area} sq ft</Text>
-          </View>
-          <View style={styles.infoItem}>
-            <Feather
-              name="map-pin"
-              size={16}
-              color="#333"
-              style={styles.icon}
-            />
-            <Text style={styles.infoText}>{address}</Text>
+            <Text style={styles.infoText}>{120}</Text>
           </View>
           <View style={styles.infoItem}>
             <Feather
@@ -42,10 +47,11 @@ const FavoriteHomesCard = ({ home }) => {
               color="#333"
               style={styles.icon}
             />
-            <Text style={styles.infoText}>{price}</Text>
+            <Text style={styles.infoText}>{"7000"}</Text>
           </View>
-          <View style={styles.dealType}>
-            <Text style={styles.dealTypeText}>{deal_type}</Text>
+          <View style={styles.infoItem}>
+            <Feather name="book" size={16} color="#333" style={styles.icon} />
+            <Text style={styles.dealTypeText}>{"کرایه"}</Text>
           </View>
         </View>
       </View>
@@ -59,21 +65,51 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     flex: 1,
-    height: 180,
+    height: 120,
     width: Dimensions.get("window").width - 40,
     alignItems: "center",
     justifyContent: "space-around",
     backgroundColor: COLORS.lightWhite,
+    borderRadius: 5,
+    overflow: "hidden",
+    elevation: 3,
+    gap: 5,
+    margin: 10,
   },
   image: {
-    height: 100,
-    width: 100,
+    height: "100%",
     flex: 1,
   },
   infoContainer: {
     flex: 1,
+    height: "100%",
+    paddingVertical: 5,
+    width: "100%",
+  },
+  infoHeaderContainer: {
+    alignItems: "flex-end",
+    paddingHorizontal: 5,
+    borderBottomColor: "#eee",
+    borderBottomWidth: 1,
+    paddingBottom: 5,
+  },
+  availabilityCircle: {
+    height: 10,
+    width: 10,
+    borderRadius: 6,
+    alignSelf: "flex-start",
   },
   detailsContainer: {
     flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flex: 1,
+    width: "100%",
+    paddingRight: 5,
+  },
+  infoItem: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
   },
 });
