ff25024f07e8ae852d67adcc2564181728f0fae2
Habib <habiburahmanpayenda@gmail.com>
2023-06-09 16:22:42 +0430

    Changes styles in home page welcome component

--- components/home/WelcomeMessage/WelcomeMessage.jsx
+++ components/home/WelcomeMessage/WelcomeMessage.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
 import SearchInput from "../../common/SearchInput/SearchInput";
+import { LinearGradient } from "expo-linear-gradient";
 
 const WelcomeMessage = ({ name }) => {
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={["#8489c2", "#7453a0"]} style={styles.container}>
       <Text style={styles.title}>ښه راغلاست {name}</Text>
       <Text style={styles.subTitle}>د څه په لټه کې یاست؟</Text>
       <SearchInput />
-    </View>
+    </LinearGradient>
   );
 };
 
