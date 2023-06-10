0a587f9e87c6e25ba80b7823a33d893167fa1fd3
Habib <habiburahmanpayenda@gmail.com>
2023-06-10 11:50:22 +0430

    Adds styles to Search Header

--- components/searchResults/Header/index.jsx
+++ components/searchResults/Header/index.jsx
@@ -1,7 +1,7 @@
 import { Pressable, StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { COLORS } from "../../../constants";
+import { COLORS, FONT } from "../../../constants";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
@@ -16,7 +16,13 @@ const SearchResultsHeader = () => {
         style={styles.contentContainer}
       >
         <View style={styles.containerTop}>
-          <TextInput style={styles.input} placeholder="د څه په لټه کې یاست؟" />
+          <View style={styles.containerTopLeft}>
+            <Text style={styles.title}>د خوښې کور مو پیدا کړئ</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="د څه په لټه کې یاست؟"
+            />
+          </View>
           <Pressable style={styles.backIcon} onPress={() => router.back()}>
             <Ionicons name="arrow-back" size={24} color="#fafafa" />
           </Pressable>
@@ -45,21 +51,39 @@ const styles = StyleSheet.create({
   containerTop: {
     flexDirection: "row",
     paddingVertical: 5,
-    paddingHorizontal: 12,
+    paddingHorizontal: 24,
     alignItems: "center",
     justifyContent: "space-between",
     width: "100%",
   },
-  input: {
+  containerTopLeft: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    gap: 12,
+  },
+  title: {
+    fontFamily: FONT.regular,
+    color: "#fafafa",
+    marginTop: 24,
+  },
+  input: {
     backgroundColor: "#fafafa",
-    height: 30,
+    height: 25,
     elevation: 1,
     padding: 0,
     paddingHorizontal: 12,
     textAlign: "right",
+    width: "80%",
+    elevation: 1,
+    borderRadius: 12,
   },
   backIcon: {
     transform: [{ rotate: "180deg" }],
+    alignSelf: "flex-start",
+    position: "absolute",
+    right: 24,
+    top: 12,
   },
 });
