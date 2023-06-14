1c8886e5031df49cecb6b2cc1fd46888012fd4d5
Habib <habiburahmanpayenda@gmail.com>
2023-06-14 10:24:40 +0430

    Adds improvements to PopularCars FlatList and data

--- components/home/PopularCarsList/PopularCarsList.jsx
+++ components/home/PopularCarsList/PopularCarsList.jsx
@@ -4,6 +4,7 @@ import { View, Text, FlatList, Pressable } from "react-native";
 import styles from "./PopularCarsListStyles";
 import { useNavigation } from "expo-router";
 import CarCard from "../../common/cards/CarCard/CarCard";
+import { useMemo } from "react";
 
 const PopularCarsList = ({ cars }) => {
   const navigation = useNavigation();
@@ -20,18 +21,34 @@ const PopularCarsList = ({ cars }) => {
     );
   };
 
+  const shouldItemUpdate = (prevItem, nextItem) => {
+    return prevItem.data !== nextItem.data;
+  };
+
+  const memoizedCars = useMemo(() => {
+    return cars.map((car) => ({
+      ...car,
+      data: JSON.stringify(car),
+    }));
+  }, [cars]);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>ډیر لیدل شوي موټر </Text>
       </View>
       <FlatList
-        data={cars}
+        data={memoizedCars}
         horizontal
         showsHorizontalScrollIndicator={false}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
         contentContainerStyle={styles.contentContainer}
+        removeClippedSubviews={true}
+        initialNumToRender={2}
+        maxToRenderPerBatch={4}
+        inverted
+        shouldItemUpdate={shouldItemUpdate}
       />
     </View>
   );
